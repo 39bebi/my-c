8126c50a71fc1c47333c408f273385ead7d70449
39bebi <babylavanya.r@gmail.com>
2025-05-13 16:17:01 +0530

    Create difference.c

---
+++ difference.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter a value of a and b\n");
+    scanf("%d%d",&a,&b);
+    printf("sum=%d\n",a+b);
+    printf("difference=%d\n",a-b);
+    printf("product=%d\n",a*b);
+    printf("quotient=%d\n",a/b);
+}
