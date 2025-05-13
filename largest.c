0d97601c90947a34cb45c373de4e3a92b80b0806
39bebi <babylavanya.r@gmail.com>
2025-05-13 16:01:11 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enetr a value of a and b\n");
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf("a is largest\n");
+    }else{
+        printf("b is largest\n");
+    }
+}
