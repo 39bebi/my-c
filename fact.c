c6204ee1bc5d8abfcb8fc433d7e4aa52e95aacb7
39bebi <babylavanya.r@gmail.com>
2025-05-14 20:29:13 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int i;
 int my_fact(int n,int fact) {
-if(n==0){
-    printf("1");
-}
-else if(n<0){
-    printf("Enter positive number");
-}
-else{
-    for(i=0;i<=n;i++){
-        fact*=i;
+    if(n==0){
+        printf("1");
     }
-}
-return 0;
+    else if(n<0){
+        printf("Enter positive number");
+    }
+    else{
+        for(i=1;i<=n;i++){
+            fact*=i;
+        }
+    }
+    return fact;
 }
 int main(){
     int c;
-    c=my_fact(4,5);
-    printf("%d",c);
+    c=my_fact(5,4);
+    printf("factorial:%d",c);
 }
